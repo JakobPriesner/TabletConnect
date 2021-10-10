a6796e22ec5a44deecd142ab0359b25f7b093906
JakobPrie <jakob.priesner@outlook.de>
2021-10-10 21:53:58 +0200

    to be continued

--- src/server/Server.java
+++ src/server/Server.java
@@ -2,30 +2,38 @@ package server;
 
 import logic.GUI;
 import logic.Message;
+import logic.Network;
 
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Server {
 
     ServerNetwork network;
     ServerLogic logic;
     GUI gui;
+    private static final Logger log = Logger.getLogger(Network.class.getName());
 
     public static void main(String[] args) {
+        log.setLevel(Level.ALL);
         Server server = new Server();
+        log.info("Server initialized.");
         server.network = new ServerNetwork(server);
+        log.info("ServerNetwork initialized.");
         server.logic = new ServerLogic();
-        server.gui = new GUI();
+        server.startSystems();
+    }
+
+    private void startSystems(){
+        network.run();
+        log.info("ServerNetwork started.");
     }
 
     public void receivedMessage(Message m){
-        if (m.getMessageType().equals("updateFrame")){
-            gui.videoStreamWrapper((BufferedImage) m.getInformation());
-        }
-    }
-
-    public void changeMode(){
-
+        // Simply forward message
+        logic.processMessage(m);
     }
 
 }
--- src/server/ServerLogic.java
+++ src/server/ServerLogic.java
@@ -1,31 +1,95 @@
 package server;
 
+import logic.GUI;
 import logic.Message;
+import logic.Network;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.logging.Logger;
 
 public class ServerLogic{
 
-    Robot robot;
-    Rectangle screen;
-
-    //toDo: translate screen sizes
+    private Robot robot;
+    private GUI gui;
+    private Rectangle screen;
+    private MouseEventHandler mvHandler;
+    private static final Logger log = Logger.getLogger(Network.class.getName());
 
     public ServerLogic(){
         try {
             this.robot = new Robot();
+            this.gui = new GUI();
+            log.info("Gui initialized.");
             this.screen = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            this.mvHandler = new MouseEventHandler();
+            log.info("MouseEventHandler initialized.");
         } catch (AWTException e) {
             e.printStackTrace(); //toDo: Exception-Handling
         }
     }
 
     public void processMessage(Message m){
-        return;
+        if (m.getMessageType().equals("initType")){
+            if (! (m.getInformation() instanceof String)){
+                log.warning("Incorrect message received!");
+                gui.criticalErrorMessage(
+                        "fataler Fehler!",
+                        "Es ist ein fataler Fehler in dem Program aufgetreten. Bitte starte es auf dem Server und Client neu.",
+                        "Message type and message information do not match!"
+                );
+            } else if (m.getInformation().equals("streamWindow")){
+                gui.runVideoStreamHandler();
+            } else if (m.getInformation().equals("streamWindowAndMouseActions")){
+                gui.runVideoStreamHandler();
+
+            }
+            if (m.getMessageType().equals("updateFrame")){
+                if (! (m.getInformation() instanceof BufferedImage)){
+                    log.warning("Incorrect message received!");
+                    gui.criticalErrorMessage(
+                            "fataler Fehler!",
+                            "Es ist ein fataler Fehler in dem Program aufgetreten. Bitte starte es auf dem Server und Client neu.",
+                            "Message type and message information do not match!"
+                    );
+                }
+                gui.addImgToVideoStreamHandler((BufferedImage) m.getInformation());
+            }
+
+        } else if (m.getMessageType().equals("updateMouseEvent")){
+            if (! (m.getInformation() instanceof MouseEvent)){
+                log.warning("Incorrect message received!");
+                gui.criticalErrorMessage(
+                        "fataler Fehler!",
+                        "Es ist ein fataler Fehler in dem Program aufgetreten. Bitte starte es auf dem Server und Client neu.",
+                        "Message type and message information do not match!"
+                );
+            }
+            addMouseEventToHandler((MouseEvent) m.getInformation());
+        }
+    }
+
+    public void addMouseEventToHandler(MouseEvent event){
+        mvHandler.mouseEvents.add(event);
     }
 
     public BufferedImage getScreenImage(){
         return robot.createScreenCapture(screen);
+    }
+
+    private class MouseEventHandler implements Runnable{
+
+        ArrayList<MouseEvent> mouseEvents;
+
+        public MouseEventHandler(){
+            mouseEvents = new ArrayList<>();
+        }
+
+        @Override
+        public void run() {
+
+        }
     }
 }
